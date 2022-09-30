8ead16ba949b8c3264223113a2333d37c3686ea8
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-09-30 17:23:41 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,11 +4,6 @@ const form = document.querySelector('form');
 const email = document.querySelector('[name=email]');
 const message = document.querySelector('[name=message]');
 
-const currentForm = {
-  mail: '',
-  mes: '',
-};
-
 try {
   const savedForm = JSON.parse(localStorage.getItem('feedback-form-state'));
   email.value = savedForm.mail;
@@ -18,8 +13,10 @@ try {
 form.addEventListener(
   'input',
   throttle(function () {
-    currentForm.mail = email.value;
-    currentForm.mes = message.value;
+    const currentForm = {
+      mail: email.value,
+      mes: message.value,
+    };
     localStorage.setItem('feedback-form-state', JSON.stringify(currentForm));
     console.log(localStorage);
   }, 500)
@@ -27,7 +24,7 @@ form.addEventListener(
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  console.log(currentForm);
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   localStorage.clear();
   form.reset();
 });
